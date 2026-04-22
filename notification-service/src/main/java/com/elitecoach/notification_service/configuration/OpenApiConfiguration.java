26e4b219f2c401febc90af5137d24e133d869b92
HenryTech12 <fakorodehenry@gmail.com>
2026-04-22 05:44:33 +0100

    updated swagger-ui documentation

--- notification-service/src/main/java/com/elitecoach/notification_service/configuration/OpenApiConfiguration.java
+++ notification-service/src/main/java/com/elitecoach/notification_service/configuration/OpenApiConfiguration.java
@@ -29,7 +29,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
                         description = "Local development server"
                 ),
                 @Server(
-                        url = "https://elitecoach.onrender.com",
+                        url = "https://elitecoach-ai-1.onrender.com",
                         description = "Production development server"
                 )
         })
