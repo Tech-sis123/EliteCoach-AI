8b75e116742a637c651295639f7d29df2535deb4
Fakorode Odunayo Henry <156727288+HenryTech12@users.noreply.github.com>
2026-04-24 18:39:52 +0100

    Update CORS_ORIGINS to include port 8080

--- core/config.py
+++ core/config.py
@@ -38,7 +38,7 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     
     # CORS
-    CORS_ORIGINS: List[str] = ["http://localhost:3000", "http://localhost:8000","https://elite-coach-seven.vercel.app", "http://localhost:8001"]
+    CORS_ORIGINS: List[str] = ["http://localhost:3000", "http://localhost:8000","https://elite-coach-seven.vercel.app", "http://localhost:8080"]
     
     # Environment
     ENVIRONMENT: str = "development"
