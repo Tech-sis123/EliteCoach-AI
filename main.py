3ae10f0ee29180047d4c6876264b0116f2989d71
HenryTech12 <fakorodehenry@gmail.com>
2026-04-29 15:47:03 +0100

    fixed cors issue

--- main.py
+++ main.py
@@ -26,8 +26,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
-    allow_origin_regex=r"^https?://(localhost|127\.0\.0\.1)(:\d+)?$",
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
