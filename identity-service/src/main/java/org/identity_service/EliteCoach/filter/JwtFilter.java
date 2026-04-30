7f81803e7b73f3f8dfdedb4eee5bc5d3f78a5ddc
HenryTech12 <fakorodehenry@gmail.com>
2026-04-30 02:38:25 +0100

    fix jwt mismatch issue

--- identity-service/src/main/java/org/identity_service/EliteCoach/filter/JwtFilter.java
+++ identity-service/src/main/java/org/identity_service/EliteCoach/filter/JwtFilter.java
@@ -62,4 +62,10 @@ public class JwtFilter extends OncePerRequestFilter {
         }
         filterChain.doFilter(request,response);
     }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getServletPath();
+        return path.startsWith("/api/v1/auth");
+    }
 }
